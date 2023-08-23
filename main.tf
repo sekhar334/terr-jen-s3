e00c7784e415094853561cc5f0d95394bbb31c65
sekhar334 <137845373+sekhar334@users.noreply.github.com>
2023-08-23 12:10:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1" # Change this to your desired AWS region
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name"
+resource "aws_s3_bucket" "example" {
+  bucket = "my-terraform-bucket" # Change this to your desired bucket name
   acl    = "private"
 }
