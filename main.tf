a5fb70c8e4ff856581ab8047171e74219732ab4f
sekhar334 <137845373+sekhar334@users.noreply.github.com>
2023-08-23 11:59:00 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region = "us-east-1" # Change this to your desired AWS region
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-unique-bucket-name"
+  acl    = "private"
+}
